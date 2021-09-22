565c663bb5058e2296ca6cb693e6dac2cc37af03
mays4 <maysalani84@yahoo.com>
2021-09-22 16:40:22 -0400

    changed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  let cols = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    cols[i] = "";
+  }
+  
+  for (let m of letters) {
+    for (let j = 0; j < m.length; j++) {
+      cols[j] += m[j];
+    }
+  }
+  
+  for (let n of cols) {
+    if (n.includes(word)) return true;
+  }
+  
+  return false;
+};
+  
+module.exports = wordSearch;
